a88ba0b09d1a209c96bae82f9a85c597ff77c257
sally_xu@hotmail.com <sally_xu@hotmail.com>
2019-11-27 21:53:20 -0500

    rename

---
+++ WebApiTest/Controllers/DotNetController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace dotnetapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DotNetController : ControllerBase
+    {
+        [HttpGet("{n:int?}")]
+        public ActionResult<IEnumerable<string>> Get(int n = 10)
+        {
+            var list = new List<string>();   
+            for (var i = 0; i < n; i++)
+            {
+                list.Add("Test" + i);
+            }
+            return Ok(list);
+        }
+    }
+}
