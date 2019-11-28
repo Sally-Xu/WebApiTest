6b2ba463f881ac197ad0e1740134a24559b03c34
sally_xu@hotmail.com <sally_xu@hotmail.com>
2019-11-27 22:47:39 -0500

    trigger build

--- WebApiTest/Controllers/DotNetController.cs
+++ WebApiTest/Controllers/DotNetController.cs
@@ -15,11 +15,12 @@ namespace dotnetapi.Controllers
         public ActionResult<IEnumerable<string>> Get(int n = 10)
         {
             var list = new List<string>();  
-             
+
             for (var i = 0; i < n; i++)
             {
                 list.Add("Test" + i);
             }
+            
             return Ok(list);
         }
     }
